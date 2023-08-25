ebda8d04249b9041cf8ef93546447c0de4ab90de
Qiuyi LI <qiuyi.li@gmail.com>
2023-08-25 11:37:40 +0200

    Add missing chinese translation

--- src/locales/lang/zh-CN/data.ts
+++ src/locales/lang/zh-CN/data.ts
@@ -84,6 +84,7 @@ export default {
     generateShippingInvoice: "生成物流发票文件",
     invoiceNumber: "发票号码",
     invoicingPage: "生成发票页",
+    invoiceType: "发票类型",
     logisticChannel: "物流渠道",
     logisticChannelCode: "渠道代码",
     logisticChannelName: "渠道名称",
