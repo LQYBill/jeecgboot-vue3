1535d931a895ee5453f24ca901a4b8c00f6139bf
Gauthier LO <lo.gauthier.glo@gmail.com>
2024-04-29 10:27:53 +0200

    (WIP) Dashboard Package status

--- src/views/dashboard/Analysis/homePage/IndexDef.vue
+++ src/views/dashboard/Analysis/homePage/IndexDef.vue
@@ -1,33 +1,33 @@
 <template>
   <div class="p-4">
-    <GrowCard :loading="loading" class="enter-y" :isEmployee="test"/>
+    <GrowCard :loading="loading" class="enter-y" :isEmployee="isEmployee"/>
 <!--    <SiteAnalysis class="!my-4 enter-y" :loading="loading" />-->
 <!--    <div class="md:flex enter-y">-->
 <!--      <VisitRadar class="md:w-1/3 w-full" :loading="loading" />-->
 <!--      <VisitSource class="md:w-1/3 !md:mx-4 !md:my-0 !my-4 w-full" :loading="loading" />-->
-<!--      <SalesProductPie class="md:w-1/3 w-full" :loading="loading" />-->
+<!--      <SalesProductPie class="md:w-1/3 w-full" :loading="loading" :isEmployee="isEmployee"/>-->
 <!--    </div>-->
   </div>
 </template>
 <script lang="ts" setup>
 import {computed, ref} from 'vue';
-  import GrowCard from '../components/GrowCard.vue';
-  import SiteAnalysis from '../components/SiteAnalysis.vue';
-  import VisitSource from '../components/VisitSource.vue';
-  import VisitRadar from '../components/VisitRadar.vue';
-  import SalesProductPie from '../components/SalesProductPie.vue';
+import GrowCard from '../components/GrowCard.vue';
+import SiteAnalysis from '../components/SiteAnalysis.vue';
+import VisitSource from '../components/VisitSource.vue';
+import VisitRadar from '../components/VisitRadar.vue';
+import SalesProductPie from '../components/SalesProductPie.vue';
 
-  const loading = ref(true);
+const loading = ref(true);
 
-  const props = defineProps({
-    isEmployee: {
-      type: Boolean,
-    },
-  })
-  const test = computed(() => {
-    return props.isEmployee;
-  })
-  setTimeout(() => {
-    loading.value = false;
-  }, 500);
+const props = defineProps({
+  isEmployee: {
+    type: Boolean,
+  },
+})
+const isEmployee = computed(() => {
+  return props.isEmployee;
+})
+setTimeout(() => {
+  loading.value = false;
+}, 500);
 </script>
