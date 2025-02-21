bdb0e23f84b996f6f22ee254c841e8de0d64680c
Gauthier LO <lo.gauthier.glo@gmail.com>
2025-02-21 11:16:32 +0100

    feat : invoice tests

--- src/views/test/invoice/data.ts
+++ src/views/test/invoice/data.ts
@@ -1,8 +1,16 @@
 import {defHttp} from "@/utils/http/axios";
 
 export const API = {
+  SHIPPING_INVOICE: '/shippingInvoice/makeShippingTest',
   PURCHASE_INVOICE: '/shippingInvoice/makePurchaseTest',
+  COMPLETE_INVOICE: '/shippingInvoice/makeCompleteTest',
+}
+export const makeShippingTest = (qty: number) => {
+  return defHttp.post({url: API.SHIPPING_INVOICE, params: qty}, { joinParamsToUrl: true });
 }
 export const makePurchaseTest = (qty: number) => {
   return defHttp.post({url: API.PURCHASE_INVOICE, params: qty}, { joinParamsToUrl: true });
+}
+export const makeCompleteTest = (qty: number) => {
+  return defHttp.post({url: API.COMPLETE_INVOICE, params: qty}, { joinParamsToUrl: true });
 }
