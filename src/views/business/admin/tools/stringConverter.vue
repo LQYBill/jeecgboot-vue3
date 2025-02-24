c38101267a5861c03487844ef4dac67cfaf806c0
Gauthier LO <lo.gauthier.glo@gmail.com>
2025-02-24 16:19:41 +0100

    feat : string converter

--- src/views/business/admin/tools/stringConverter.vue
+++ src/views/business/admin/tools/stringConverter.vue
@@ -17,7 +17,6 @@
                             v-model:value="formState.input"
                             placeholder="Input string"
                             allowClear
-                            @blur="handleInputBlur"
                             :rows="10"
                 ></a-textarea>
               </a-form-item>
@@ -34,21 +33,40 @@
                             placeholder="Output string"
                             class="disabled:cursor-text"
                             disabled
+                            :rows="10"
                 ></a-textarea>
               </a-form-item>
+            </a-col>
+            <a-col>
+              <a-button-group>
+                <a-button type="primary" preIcon="ant-design:copy-outlined" @click="handleCopy(formState.output)">
+                  {{ t('common.operation.copy') }}
+                </a-button>
+                <a-button type="success" @click="handleDownloadExcel">
+                  {{ `${t('common.operation.download')} Excel` }}
+                </a-button>
+              </a-button-group>
             </a-col>
           </a-row>
         </a-form>
       </a-card>
-      <settings></settings>
+      <settings @update="handleUpdate"></settings>
     </div>
 </PageWrapper>
 </template>
 <script lang="ts" setup>
 import {PageWrapper} from "/@/components/Page"
-import { ref, reactive } from "vue";
+import {ref, reactive, provide, unref} from "vue";
 import {Form} from "ant-design-vue";
 import Settings from "./components/settings.vue";
+import {useMessage} from "@/hooks/web/useMessage";
+import {useI18n} from "@/hooks/web/useI18n";
+import {useCopyToClipboard} from "@/hooks/web/useCopyToClipboard";
+import {downloadFile} from "@/api/common/api";
+import {Api} from "@/views/business/admin/tools/data";
+const {t} = useI18n();
+const { clipboardRef, copiedRef } = useCopyToClipboard();
+const { createMessage } = useMessage();
 
 const formRef = ref();
 const useForm = Form.useForm;
@@ -61,7 +79,25 @@ const formState = reactive<Record<string, string>>({
 });
 const { validateInfos } = useForm(formState, validatorRules, { immediate: false });
 
-function handleInputBlur() {
-  console.log('input:', formState.input);
+function handleUpdate(res: string) {
+  formState.output = res;
 }
+function handleCopy(numbers:string) {
+  if (!numbers) {
+    createMessage.warning(t('component.copy.noValue'));
+    return;
+  }
+  clipboardRef.value = numbers;
+  if (unref(copiedRef)) {
+    createMessage.warning(t('component.copy.success'));
+  }
+}
+function handleDownloadExcel() {
+  const params = {
+    input: formState.output,
+  }
+  const filename = "Feuille.xlsx";
+  downloadFile(Api.EXCEL, filename, params);
+}
+provide('state', formState);
 </script>
