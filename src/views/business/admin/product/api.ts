bd26a54b871dbccf22e159efce0a4a89d8aa6d1a
Xue YANG <xuey648@gmail.com>
2025-06-03 12:17:46 +0200

    feat: add constraints and notification for SKU weight updates

--- src/views/business/admin/product/api.ts
+++ src/views/business/admin/product/api.ts
@@ -2,7 +2,7 @@ import {defHttp} from "@/utils/http/axios";
 import {Api} from "@/views/business/admin/product/data";
 
 export const listSkus = async (params, handler: Function) => {
-  await defHttp.get({url: Api.list, params}).then(async (res) => {
+  await defHttp.get({url: Api.listAll, params}).then(async (res) => {
     await handler(res);
   });
 };
