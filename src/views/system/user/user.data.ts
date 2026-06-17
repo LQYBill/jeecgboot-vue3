dd988270f620e16a0acbffc9b72f67ab5654d872
Qiuyi LI <qiuyi.li@gmail.com>
2026-06-17 17:42:57 +0200

    Make user role non mandatory field

--- src/views/system/user/user.data.ts
+++ src/views/system/user/user.data.ts
@@ -190,7 +190,7 @@ export const formSchema: FormSchema[] = [
   {
     label: '角色',
     field: 'selectedroles',
-    required: true,
+    required: false,
     component: 'ApiSelect',
     componentProps: {
       mode: 'multiple',
